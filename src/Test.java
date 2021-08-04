e696c7fb37dd393fabea213cdeda3d436092f2ea
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-08-04 13:50:55 +0700

    variable

--- src/Test.java
+++ src/Test.java
@@ -4,23 +4,24 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Nhập vào giá trị");
         Scanner sc = new Scanner(System.in);
+//        char result = sc.next().charAt(0);
         String result = sc.nextLine();
+        System.out.println(result.equals("A"));
         switch (result) {
-            case "A":
-                System.out.println("Loại A");
-                break;
-            case "B":
-                System.out.println("Loại B");
-                break;
-            case "C":
-                System.out.println("Loại C");
-                break;
-            case "D":
-                System.out.println("Loại D");
-                break;
-            default:
-                System.out.println("Loại E");
-        }
-        System.out.println(result == "A");
+                case "A":
+                    System.out.println("Loại A");
+                    break;
+                case "B":
+                    System.out.println("Loại B");
+                    break;
+                case "C":
+                    System.out.println("Loại C");
+                    break;
+                case "D":
+                    System.out.println("Loại D");
+                    break;
+                default:
+                    System.out.println("Loại E");
+            }
     }
 }
---
+++ src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5F;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
