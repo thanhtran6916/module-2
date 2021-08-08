6d43f73ab7417d2bbcfc6934bbf243e488115898
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-08-08 10:57:25 +0700

    Fan

---
+++ src/Fan/Fan.java
@@ -0,0 +1,66 @@
+package Fan;
+
+public class Fan {
+    final int slow = 1;
+    final int medium = 2;
+    final int fast = 3;
+    private int speed = medium;
+    private boolean onOff = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public Fan() {
+    }
+
+    public int getSlow() {
+        return this.slow;
+    }
+
+    public boolean getOnOff() {
+        return this.onOff;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int setSlow(int number) {
+        this.speed = number;
+        return this.speed;
+    }
+
+    public boolean setOnOff(boolean status) {
+        this.onOff = status;
+        return this.onOff;
+    }
+
+    public double setRadius(double number) {
+        this.radius = number;
+        return this.radius;
+    }
+
+    public String setColor(String color) {
+        this.color = color;
+        return this.color;
+    }
+
+    public String toString() {
+        String result = "";
+        if (this.onOff) {
+            result += "speed: " + this.speed + "\n"
+                    + "color: " + this.color + "\n"
+                    + "radius: " + this.radius + "\n"
+                    + "fan is on!";
+        } else {
+            result += "color: " + this.color + "\n"
+                    + "radius: " + this.radius + "\n"
+                    + "fan is off!";
+        }
+        return result;
+    }
+
+}
---
+++ src/Fan/main.java
@@ -0,0 +1,20 @@
+package Fan;
+
+public class main {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.setSlow(fan1.fast);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOnOff(true);
+        System.out.println("Fan1");
+        System.out.println(fan1.toString());
+
+        System.out.println("");
+
+        System.out.println("Fan2"   );
+        Fan fan2 = new Fan();
+        fan2.setSlow(fan2.medium);
+        System.out.println(fan2.toString());
+    }
+}
