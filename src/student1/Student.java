a3ff209bef92f9efdf24ef8c45e0ca86253223fe
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-08-10 14:40:31 +0700

    a

---
+++ src/student1/Student.java
@@ -0,0 +1,16 @@
+package student1;
+
+public class Student {
+    private String name;
+    private String classes;
+
+    public Student() {}
+
+    public String setName(String name) {
+        return this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ src/student1/Test.java
@@ -0,0 +1,9 @@
+package student1;
+
+public class Test {
+    public static void main(String[] args) {
+        Student a = new Student();
+        a.setClasses("a");
+        a.setClasses("a");
+    }
+}
